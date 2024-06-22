2b33bffb62cab494a9a1a3de1e73057c6f94953d
danielmichaelni <danielmichaelni@gmail.com>
2024-06-22 19:28:36 -0400

    use plasmo storage api

--- src/contents/binge.ts
+++ src/contents/binge.ts
@@ -1,9 +1,13 @@
 import type { PlasmoCSConfig } from "plasmo"
 
+import { Storage } from "@plasmohq/storage"
+
 export const config: PlasmoCSConfig = {
   matches: ["https://*.rapid-cloud.co/*"],
   all_frames: true
 }
+
+const storage = new Storage()
 
 const createButtonClickAttempt = (id: string) => () => {
   const button = document.querySelector<HTMLElement>(`a[id*="${id}"]`)
@@ -25,85 +29,74 @@ const adjustPlaybackRate = (playbackRate: number) => {
   }
 }
 
-const main = () => {
-  chrome.storage.sync.get(
-    {
-      skipIntroEnabled: true,
-      skipOutroEnabled: true,
-      changeSpeedEnabled: true,
-      speed: 1
-    },
-    ({ skipIntroEnabled, skipOutroEnabled, changeSpeedEnabled, speed }) => {
-      let isSkipIntroEnabled = JSON.parse(skipIntroEnabled)
-      let isSkipOutroEnabled = JSON.parse(skipOutroEnabled)
-      let isChangeSpeedEnabled = JSON.parse(changeSpeedEnabled)
-      let playbackRate = JSON.parse(speed)
+const main = async () => {
+  let isSkipIntroEnabled =
+    (await storage.get<boolean>("skipIntroEnabled")) || true
+  let isSkipOutroEnabled =
+    (await storage.get<boolean>("skipOutroEnabled")) || true
+  let isChangeSpeedEnabled =
+    (await storage.get<boolean>("changeSpeedEnabled")) || true
+  let playbackRate = (await storage.get<number>("speed")) || 1
 
+  storage.watch({
+    skipIntroEnabled: (c) => {
+      isSkipIntroEnabled = c.newValue
+    },
+    isSkipOutroEnabled: (c) => {
+      isSkipOutroEnabled = c.newValue
+    },
+    isChangeSpeedEnabled: (c) => {
+      isChangeSpeedEnabled = c.newValue
       if (isChangeSpeedEnabled) {
         adjustPlaybackRate(playbackRate)
+      } else {
+        adjustPlaybackRate(1)
       }
-
-      console.log("add event listener")
-      addEventListener("keydown", (event) => {
-        console.log("is change speed enabled", isChangeSpeedEnabled)
-        if (!isChangeSpeedEnabled) {
-          return
-        }
-
-        const target = event.target as HTMLElement
-        if (
-          target.nodeName === "INPUT" ||
-          target.nodeName === "TEXTAREA" ||
-          target.isContentEditable
-        ) {
-          return
-        }
-
-        if (event.key === "s") {
-          chrome.storage.sync.set({ speed: Math.max(0, playbackRate - 0.1) })
-        }
-        if (event.key === "d") {
-          chrome.storage.sync.set({ speed: playbackRate + 0.1 })
-        }
-      })
-
-      chrome.storage.onChanged.addListener(
-        ({ skipIntroEnabled, skipOutroEnabled, changeSpeedEnabled, speed }) => {
-          if (skipIntroEnabled) {
-            isSkipIntroEnabled = JSON.parse(skipIntroEnabled?.newValue)
-          }
-          if (skipOutroEnabled) {
-            isSkipOutroEnabled = JSON.parse(skipOutroEnabled?.newValue)
-          }
-          if (changeSpeedEnabled) {
-            isChangeSpeedEnabled = JSON.parse(changeSpeedEnabled?.newValue)
-          }
-          if (speed) {
-            playbackRate = JSON.parse(speed?.newValue)
-          }
-
-          if (isChangeSpeedEnabled) {
-            adjustPlaybackRate(playbackRate)
-          } else {
-            adjustPlaybackRate(1)
-          }
-        }
-      )
-
-      const observer = new MutationObserver(() => {
-        if (isSkipIntroEnabled) {
-          skipIntroAttempt()
-        }
-        if (isSkipOutroEnabled) {
-          skipOutroAttempt()
-        }
-        if (isChangeSpeedEnabled) {
-          adjustPlaybackRate(playbackRate)
-        }
-      })
-      observer.observe(document.body, { childList: true, subtree: true })
+    },
+    speed: (c) => {
+      playbackRate = c.newValue
+      adjustPlaybackRate(playbackRate)
     }
-  )
+  })
+
+  if (isChangeSpeedEnabled) {
+    adjustPlaybackRate(playbackRate)
+  }
+
+  addEventListener("keydown", async (event) => {
+    if (!isChangeSpeedEnabled) {
+      return
+    }
+
+    const target = event.target as HTMLElement
+    if (
+      target.nodeName === "INPUT" ||
+      target.nodeName === "TEXTAREA" ||
+      target.isContentEditable
+    ) {
+      return
+    }
+
+    if (event.key === "s") {
+      await storage.set("speed", Math.max(0, playbackRate - 0.1))
+    }
+    if (event.key === "d") {
+      await storage.set("speed", playbackRate + 0.1)
+    }
+  })
+
+  const observer = new MutationObserver(() => {
+    if (isSkipIntroEnabled) {
+      skipIntroAttempt()
+    }
+    if (isSkipOutroEnabled) {
+      skipOutroAttempt()
+    }
+    if (isChangeSpeedEnabled) {
+      adjustPlaybackRate(playbackRate)
+    }
+  })
+  observer.observe(document.body, { childList: true, subtree: true })
 }
 
 window.addEventListener("load", () => {
